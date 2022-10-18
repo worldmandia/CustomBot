3115053d083d6b57578db018e415f976f515e365
mani1232 <perederkoura@gmail.com>
2022-10-18 22:17:33 +0200

    todo

--- src/main/java/ua/mani123/discord/action/filter/Filter.java
+++ src/main/java/ua/mani123/discord/action/filter/Filter.java
@@ -4,6 +4,10 @@ import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 
 public interface Filter {
 
+    /*
+      TODO users filter
+     */
+
     /**
      * Check actions filters and return true if can or false if cant
      *
