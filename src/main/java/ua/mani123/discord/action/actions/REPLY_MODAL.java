8536d6af6af25366a44191adb94dec033b11d940
mani1232 <perederkoura@gmail.com>
2022-12-24 00:22:13 +0200

    TODO

---
+++ src/main/java/ua/mani123/discord/action/actions/REPLY_MODAL.java
@@ -0,0 +1,26 @@
+package ua.mani123.discord.action.actions;
+
+import java.util.ArrayList;
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
+import org.apache.commons.text.StringSubstitutor;
+import ua.mani123.discord.action.Action;
+import ua.mani123.discord.action.filter.Filter;
+
+// TODO REPLY_MODAL
+public class REPLY_MODAL implements Action {
+
+  @Override
+  public void run(GenericInteractionCreateEvent event) {
+
+  }
+
+  @Override
+  public void runWithPlaceholders(GenericInteractionCreateEvent event, StringSubstitutor str) {
+
+  }
+
+  @Override
+  public ArrayList<Filter> getFilters() {
+    return Action.super.getFilters();
+  }
+}
